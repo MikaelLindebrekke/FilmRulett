d7869292651b22c383908cc45c5ff5d35a5152ff
MikaelLindebrekke <lindebrekkem@gmail.com>
2022-07-09 16:09:57 +0200

    server

---
+++ server.js
@@ -0,0 +1,9 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('database.json');
+const midlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(midlewares);
+server.use(router);
+server.listen(port);
